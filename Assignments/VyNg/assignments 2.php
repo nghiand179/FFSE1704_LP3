c70c898130cec809ee60dd1c5219fd2d3b4c771a
davidvy <36950456+davidvy@users.noreply.github.com>
2018-03-06 16:05:11 +0700

    upday assign 2

---
+++ Assignments/VyNg/assignments 2.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<h1>Giải phương trình bậc hai(có dạng ax2 + bx + c =0 𝑣ớ𝑖 a ≠ 0)</h1>
+<table border="1px" cellpadding="5px" cellspacing="0px" style="width:50%;">
+	<tr bgcolor = #d6d6d6>
+		<td style="text-align: center;">TT</td>
+		<td style="text-align: right;">a</td>
+		<td style="text-align: right;">b</td>
+		<td style="text-align: right;">c</td>
+		<td style="text-align: right;">x1</td>
+		<td style="text-align: right;">x2</td>
+		<td style="text-align: center;">Ghi chú</td>
+	</tr>
+	<tr>
+		<td style="text-align: center;"><?=$tt1?></td>
+		<td style="text-align: right;"><?=$a1?></td>
+		<td style="text-align: right;"><?=$b1?></td>
+		<td style="text-align: right;"><?=$c1?></td>
+		<td style="text-align: right;"><?=$x11?></td>
+		<td style="text-align: right;"><?=$x12?></td>
+		<td style="text-align: left;"><?=$ghi_chu1?></td>
+	</tr>
+	<tr>
+		<td style="text-align: center;"><?=$tt1?></td>
+		<td style="text-align: right;"><?=$a1?></td>
+		<td style="text-align: right;"><?=$b1?></td>
+		<td style="text-align: right;"><?=$c1?></td>
+		<td style="text-align: right;"><?=$x11?></td>
+		<td style="text-align: right;"><?=$x12?></td>
+		<td style="text-align: left;"><?=$ghi_chu1?></td>
+	</tr>
+	<tr>
+		<td style="text-align: center;"><?=$tt1?></td>
+		<td style="text-align: right;"><?=$a1?></td>
+		<td style="text-align: right;"><?=$b1?></td>
+		<td style="text-align: right;"><?=$c1?></td>
+		<td style="text-align: right;"><?=$x11?></td>
+		<td style="text-align: right;"><?=$x12?></td>
+		<td style="text-align: left;"><?=$ghi_chu1?></td>
+	</tr>
+	<tr>
+		<td style="text-align: center;"><?=$tt1?></td>
+		<td style="text-align: right;"><?=$a1?></td>
+		<td style="text-align: right;"><?=$b1?></td>
+		<td style="text-align: right;"><?=$c1?></td>
+		<td style="text-align: right;"><?=$x11?></td>
+		<td style="text-align: right;"><?=$x12?></td>
+		<td style="text-align: left;"><?=$ghi_chu1?></td>
+	</tr>
+	<tr>
+		<td style="text-align: center;"><?=$tt1?></td>
+		<td style="text-align: right;"><?=$a1?></td>
+		<td style="text-align: right;"><?=$b1?></td>
+		<td style="text-align: right;"><?=$c1?></td>
+		<td style="text-align: right;"><?=$x11?></td>
+		<td style="text-align: right;"><?=$x12?></td>
+		<td style="text-align: left;"><?=$ghi_chu1?></td>
+	</tr>
+	<tr>
+		<td style="text-align: center;"><?=$tt1?></td>
+		<td style="text-align: right;"><?=$a1?></td>
+		<td style="text-align: right;"><?=$b1?></td>
+		<td style="text-align: right;"><?=$c1?></td>
+		<td style="text-align: right;"><?=$x11?></td>
+		<td style="text-align: right;"><?=$x12?></td>
+		<td style="text-align: left;"><?=$ghi_chu1?></td>
+	</tr>
+	<tr>
+		<td style="text-align: center;"><?=$tt1?></td>
+		<td style="text-align: right;"><?=$a1?></td>
+		<td style="text-align: right;"><?=$b1?></td>
+		<td style="text-align: right;"><?=$c1?></td>
+		<td style="text-align: right;"><?=$x11?></td>
+		<td style="text-align: right;"><?=$x12?></td>
+		<td style="text-align: left;"><?=$ghi_chu1?></td>
+	</tr>
+	<tr>
+		<td style="text-align: center;"><?=$tt1?></td>
+		<td style="text-align: right;"><?=$a1?></td>
+		<td style="text-align: right;"><?=$b1?></td>
+		<td style="text-align: right;"><?=$c1?></td>
+		<td style="text-align: right;"><?=$x11?></td>
+		<td style="text-align: right;"><?=$x12?></td>
+		<td style="text-align: left;"><?=$ghi_chu1?></td>
+	</tr>
+	<tr>
+		<td style="text-align: center;"><?=$tt1?></td>
+		<td style="text-align: right;"><?=$a1?></td>
+		<td style="text-align: right;"><?=$b1?></td>
+		<td style="text-align: right;"><?=$c1?></td>
+		<td style="text-align: right;"><?=$x11?></td>
+		<td style="text-align: right;"><?=$x12?></td>
+		<td style="text-align: left;"><?=$ghi_chu1?></td>
+	</tr>
+	<tr>
+		<td style="text-align: center;"><?=$tt1?></td>
+		<td style="text-align: right;"><?=$a1?></td>
+		<td style="text-align: right;"><?=$b1?></td>
+		<td style="text-align: right;"><?=$c1?></td>
+		<td style="text-align: right;"><?=$x11?></td>
+		<td style="text-align: right;"><?=$x12?></td>
+		<td style="text-align: left;"><?=$ghi_chu1?></td>
+	</tr>
+</table>
+</body>
+</html>
