f4851200937047d9d9b6ef552fe685c2b1b88275
quanhqh <32627897+quanhqh@users.noreply.github.com>
2018-03-11 23:20:10 +0700

    Assignment_PHP1

---
+++ Assignments/QuanLT/Assignment_PHP1.php
@@ -0,0 +1,365 @@
+<?php
+$a = 2.5;
+$b = 2;
+if($a==0){
+    $x= "He so khong hop le, a=0";
+    $kq= "-";
+} else {
+    $x="Phuong trinh co nghiem";
+    $kq=(-$b / ($a));
+}
+?>
+<?php
+$a2 = 0;
+$b2 = 1;
+if($a2==0){
+    $x2= "He so khong hop le, a=0";
+    $kq2= "-";
+        
+} else {
+    $x2="Phuong trinh co nghiem";
+    $kq2=(-$b2 / ($a2));
+}
+?>
+<?php
+$a3 = -1.7;
+$b3 = -9.6;
+if($a3==0){
+    $x3= "He so khong hop le, a=0";
+    $kq3= "-";
+} else {
+    $x3="Phuong trinh co nghiem";
+    $kq3=(-$b3 / ($a3));
+}
+?>
+<?php
+$a4 = 0.2;
+$b4 = 0.5;
+if($a4==0){
+    $x4= "He so khong hop le, a=0";
+    $kq4= "-";
+} else {
+    $x4="Phuong trinh co nghiem";
+    $kq4=(-$b4 / ($a4));
+}
+?>
+<?php
+$a5 = -6.2;
+$b5 = 0.3;
+if($a5==0){
+    $x5= "He so khong hop le, a=0";
+    $kq5= "-";
+} else {
+    $x5="Phuong trinh co nghiem";
+    $kq5=(-$b5 / ($a5));
+}
+?>
+<?php
+$a6 = 0.2;
+$b6 = 3.6;
+if($a6==0){
+    $x6= "He so khong hop le, a=0";
+    $kq6= "-";
+} else {
+    $x6="Phuong trinh co nghiem";
+    $kq6=(-$b6 / ($a6));
+}
+?>
+<?php
+$a7 = 9.5;
+$b7 = 6;
+if($a7==0){
+    $x7= "He so khong hop le, a=0";
+    $kq7= "-";
+} else {
+    $x7="Phuong trinh co nghiem";
+    $kq7=(-$b7 / ($a7));
+}
+?>
+<?php
+$a8 =-3.9;
+$b8 = -0.3;
+if($a8==0){
+    $x8= "He so khong hop le, a=0";
+    $kq8= "-";
+    
+} else {
+    $x8="Phuong trinh co nghiem";
+    $kq8=(-$b8 / ($a8));
+}
+?>
+<html>
+    <table border="1"cellpadding="25"cellspacing="0">
+        <tr>
+            <th>TT</th>
+            <th>a</th>
+            <th>b</th>
+            <th>x</th>
+            <th>Ghi chu</th>
+        </tr>
+        <tr>
+            <td><?php
+            if($a==0){
+            echo "<p>1</p>";
+            } else {
+               echo "1"; 
+            }?></td>
+            <td><?php
+            if($a==0){
+            echo "<p>$a</p>";
+            } else {
+               echo "$a"; 
+            }?></td>
+            <td><?php
+            if($a==0){
+            echo "<p>$b</p>";
+            } else {
+               echo "$b"; 
+            }?></td>
+            <td><?php
+            if($a==0){
+            echo "<p>$kq</p>";
+            } else {
+               echo "$kq"; 
+            }?></td>
+            <td><?php
+            if($a==0){
+            echo "<p>$x</p>";
+            } else {
+               echo "$x"; 
+            }?></td>
+        </tr>
+        <tr>
+            <td><?php
+            if($a2==0){
+            echo "<p>2</p>";
+            } else {
+               echo "2"; 
+            }?></td>
+            <td><?php
+            if($a2==0){
+            echo "<p>$a2</p>";
+            } else {
+               echo "$a2"; 
+            }?></td>
+            <td><?php
+            if($a2==0){
+            echo "<p>$b2</p>";
+            } else {
+               echo "$b2"; 
+            }?></td>
+            <td><?php
+            if($a2==0){
+            echo "<p>$kq2</p>";
+            } else {
+               echo "$kq2"; 
+            }?></td>
+            <td><?php
+            if($a2==0){
+            echo "<p>$x2</p>";
+            } else {
+               echo "$x2"; 
+            }?></td>
+        </tr>
+        <tr>
+            <td><?php
+            if($a3==0){
+            echo "<p>3</p>";
+            } else {
+               echo "3"; 
+            }?></td>
+            <td><?php
+            if($a3==0){
+            echo "<p>$a3</p>";
+            } else {
+               echo "$a3"; 
+            }?></td>
+            <td><?php
+            if($a3==0){
+            echo "<p>$b3</p>";
+            } else {
+               echo "$b3"; 
+            }?></td>
+            <td><?php
+            if($a3==0){
+            echo "<p>$kq3</p>";
+            } else {
+               echo "$kq3"; 
+            }?></td>
+            <td><?php
+            if($a3==0){
+            echo "<p>$x3</p>";
+            } else {
+               echo "$x3"; 
+            }?></td>
+        </tr>
+        <tr>
+            <td><?php
+            if($a4==0){
+            echo "<p>4</p>";
+            } else {
+               echo "4"; 
+            }?></td>
+            <td><?php
+            if($a4==0){
+            echo "<p>$a4</p>";
+            } else {
+               echo "$a4"; 
+            }?></td>
+            <td><?php
+            if($a4==0){
+            echo "<p>$b4</p>";
+            } else {
+               echo "$b4"; 
+            }?></td>
+            <td><?php
+            if($a4==0){
+            echo "<p>$kq4</p>";
+            } else {
+               echo "$kq4"; 
+            }?></td>
+            <td><?php
+            if($a4==0){
+            echo "<p>$x4</p>";
+            } else {
+               echo "$x4"; 
+            }?></td>
+            
+        </tr>
+        <tr>
+            <td><?php
+            if($a5==0){
+            echo "<p>5</p>";
+            } else {
+               echo "5"; 
+            }?></td>
+            <td><?php
+            if($a5==0){
+            echo "<p>$a5</p>";
+            } else {
+               echo "$a5"; 
+            }?></td>
+            <td><?php
+            if($a5==0){
+            echo "<p>$b5</p>";
+            } else {
+               echo "$b5"; 
+            }?></td>
+            <td><?php
+            if($a5==0){
+            echo "<p>$kq5</p>";
+            } else {
+               echo "$kq5"; 
+            }?></td>
+            <td><?php
+            if($a5==0){
+            echo "<p>$x5</p>";
+            } else {
+               echo "$x5"; 
+            }?></td>
+        </tr>
+        <tr>
+            <td><?php
+            if($a6==0){
+            echo "<p>6</p>";
+            } else {
+               echo "6"; 
+            }?></td>
+            <td><?php
+            if($a6==0){
+            echo "<p>$a6</p>";
+            } else {
+               echo "$a6"; 
+            }?></td>
+            <td><?php
+            if($a6==0){
+            echo "<p>$b6</p>";
+            } else {
+               echo "$b6"; 
+            }?></td>
+            <td><?php
+            if($a6==0){
+            echo "<p>$kq6</p>";
+            } else {
+               echo "$kq6"; 
+            }?></td>
+            <td><?php
+            if($a6==0){
+            echo "<p>$x6</p>";
+            } else {
+               echo "$x6"; 
+            }?></td>
+        </tr>
+        <tr>
+            <td><?php
+            if($a==0){
+            echo "<p>7</p>";
+            } else {
+               echo "7"; 
+            }?></td>
+            <td><?php
+            if($a7==0){
+            echo "<p>$a7</p>";
+            } else {
+               echo "$a7"; 
+            }?></td>
+            <td><?php
+            if($a7==0){
+            echo "<p>$b7</p>";
+            } else {
+               echo "$b7"; 
+            }?></td>
+            <td><?php
+            if($a7==0){
+            echo "<p>$kq7</p>";
+            } else {
+               echo "$kq7"; 
+            }?></td>       
+            <td><?php
+            if($a7==0){
+            echo "<p>$x7</p>";
+            } else {
+               echo "$x7"; 
+            }?></td>
+        </tr>
+        <tr>
+            <td><?php
+            if($a==0){
+            echo "<p>8</p>";
+            } else {
+               echo "8"; 
+            }?></td>
+            <td><?php
+            if($a8==0){
+            echo "<p>$a8</p>";
+            } else {
+               echo "$a8"; 
+            }?></td>
+            <td><?php
+            if($a8==0){
+            echo "<p>$b8</p>";
+            } else {
+               echo "$b8"; 
+            }?></td>
+            <td><?php
+            if($a8==0){
+            echo "<p>$kq8</p>";
+            } else {
+               echo "$kq8"; 
+            }?></td>
+            <td><?php
+            if($a8==0){
+            echo "<p>$x8</p>";
+            } else {
+               echo "$x8"; 
+            }?></td>
+        </tr>
+    </table>
+    <style type="text/css">
+       p {
+        color: red;
+       }
+    </style>
+</html>
+
+
