37b0d93519ebce15f9dd50d5c4e4b28ede70e646
lamnt <lnguyen24794@gmail.com>
2018-03-20 16:03:08 +0700

    PHP day 4 Upload file

---
+++ Assignments/lamnt/shop.php
@@ -0,0 +1,14 @@
+<form action='basket.php' method = 'post' enctype="multipart/form-data">
+	Id hoa: <br/>
+	<input type ='text' name='id'/><br/>
+	Tên hoa: <br/>
+	<input type ='name' name='name'/><br/>
+	Số lượng: <br/>
+	<input type ='text' name='qty'/><br/>
+	Đơn giá: <br/>
+	<input type ='text' name='price'/> <br/>
+	Hình ảnh: <br/>
+	<input type= 'file' name="hinhanh"/> <br/>
+	
+	<input type ='submit' name='muahoa' value="Mua Hoa"/>
+</form>
