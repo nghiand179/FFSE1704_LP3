a069182590712ade60bd36e1a4ccb0869354e239
ffse1704006 <36950238+ffse1704006@users.noreply.github.com>
2018-03-23 14:18:12 +0700

    zzx

--- Assignments/MinhTQ/readme.md
+++ Assignments/MinhTQ/readme.md
@@ -2,4 +2,4 @@
 
 ### Assignment List
 
-1. Assigment 0: [Hello World](https://github.com/FASTTRACKSE/FFSE1704_LP3/blob/master/Assignments/ThanhCL/hello.php)
+1. Assigment 1: [Hello World](https://github.com/FASTTRACKSE/FFSE1704_LP3/blob/master/Assignments/MinhTQ/php-asm-01.php)<br>
---
+++ Assignments/lamnt/uploadfide.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<form action="" method="post">
+	<input type="text" name="cd" value="chiều dài ">
+	<input type="text" name="cr" value="chiều rộng">
+	<input type="submit" name="submit" value="kết quả ">
+</form>              
+</body>
+</html>
